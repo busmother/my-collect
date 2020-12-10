d544a7b2118209c433d5eabdb9b94193d3385b6c
Ian Marshall <imarshall14@gmail.com>
2020-12-10 09:32:10 -0500

    commit tree 31bdd4f67d86da7ca47cc2bef44cb20a6fa3a4f5

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
-
-
+require 'pry'
+   
+def my_collect(array)
+    collection = []
+    if array.count == 0
+        return "No block received."
+    else
+        i = 0
+        while i < array.length
+          input = yield(array[i])
+          collection << input
+          i = i + 1
+        end
+    end
+    collection
+end
